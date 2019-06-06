9c49c9a1d51a19419c1106e8f775a258c56a36a7
Alex Bausk <bauskas@gmail.com>
2019-06-07 01:21:55 +0300

    Added debug config. This is half-working sapper->now commit, server routes are broken

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,3 @@
-import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
@@ -6,16 +5,13 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const app = polka() // You can also use Express
-	.use(compression({ threshold: 0 }));
+const app = polka().use(compression({ threshold: 0 }));
 
 if (dev) {
-	app.use(sirv('static', { dev }));
+	app.use(require('sirv')('static', { dev }));
 }
 
 app.use(sapper.middleware())
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
-
-module.exports = app.handler;
