78ebd17e84951b8966a987f446550757dc8bb192
Alex Bausk <bauskas@gmail.com>
2020-02-21 23:55:09 +0200

    [BACKEND] Task manager

--- backend/taskmanager/__init__.py
+++ backend/taskmanager/__init__.py
@@ -1,14 +1,28 @@
 import asyncio
 from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+from traderutils.datatypes.cache import ForwardOnceCache
+from traderutils.constants import currencies
+from traderutils.sources.historical import CryptowatchSource, KunaIoSource
+
 
 class DataManager:
     def __init__(self):
         self.tasks = {}
+        self.pool = ThreadPoolExecutor(max_workers=20)
+        self.loop = asyncio.get_event_loop()
+        self.app = None
+        self._datasources = {
+            'kunaio': KunaIoSource(currency=currencies.BTC),
+            'cryptowatch': CryptowatchSource(currency=currencies.BTC)
+        }
         self._data = {
-            'kunaio': None,
+            'kunaio': ForwardOnceCache('timestamp'),
+            'cryptowatch': ForwardOnceCache('timestamp')
         }
 
     async def start_background_tasks(self, app):
+        self.app = app
         self.tasks['data'] = app.loop.create_task(self._schedule(3, self._retrieve_data, print))
         self.tasks['signal'] = app.loop.create_task(self._schedule(5, self._calculate_signal, print))
 
@@ -25,7 +39,21 @@ class DataManager:
         return self._data
 
     async def _retrieve_data(self):
-        await asyncio.sleep(0.1)
+        for source_name, source in self._datasources.items():
+            if source:
+                future = self.loop.run_in_executor(self.pool, source.fetch_latest_trades)
+
+                def callback(fut):
+                    try:
+                        result = fut.result()
+                        current_cache = self._data[source_name]
+                        current_cache.put(result)
+                    except Exception as e:
+                        print(e)
+                    else:
+                        pass
+
+                future.add_done_callback(callback)
         return "ok"
 
     async def _calculate_signal(self):
@@ -46,7 +74,9 @@ class DataManager:
                 try:
                     result = fut.result()
                     if handler:
-                        asyncio.create_task(handler(result))
+                        possible_future = handler(result)
+                        if asyncio.iscoroutine(possible_future):
+                            asyncio.create_task(possible_future)
                 except Exception as e:
                     errors.append(e)
                 else:
