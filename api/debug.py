e8c1bbee171100e4ebf11f56558411e34b863053
Alex Bausk <bauskas@gmail.com>
2020-03-28 17:04:31 +0200

    [api] [scripts] Cleanup

--- api/debug.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-import ptvsd
-ptvsd.enable_attach()
-
-from entrypoint import *
-
-
-if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=5000)
--- scripts/develop.sh
+++
@@ -1,2 +0,0 @@
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-python ${__dir}/predevelop.py "$@"
