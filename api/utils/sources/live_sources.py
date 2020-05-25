de1353d0ac6e3c1ab2d25fd97200d3bfcb6ee603
Alex Bausk <bauskas@gmail.com>
2020-05-26 00:19:56 +0300

    [api] added live sources abstract implementations

---
+++ api/utils/sources/live_sources.py
@@ -0,0 +1,18 @@
+from .abstract_source import AbstractSource
+
+
+class LiveKunaSource(AbstractSource):
+    latest_endpoint = "https://kuna.io/api/v2/trades?market={currency}"
+
+    @classmethod
+    async def get_latest(cls):
+
+        raise NotImplementedError('Implement get_availability_intervals')
+
+
+class LiveCryptowatchSource(AbstractSource):
+    latest_endpoint = "https://api.cryptowat.ch/markets/gdax/{currency}/trades?limit={limit}&since={after}"
+
+    @classmethod
+    async def get_latest(cls):
+        raise NotImplementedError('Implement get_availability_intervals')
