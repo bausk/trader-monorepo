a11a332046bea475bcd02a5e1ebcedb96eecc285
Alexander Bausk <bauskas@gmail.com>
2021-01-10 22:14:41 +0200

    changes to fix node build

--- deploy-etl-stage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git branch -D etlmaster
-git subtree split --prefix etl -b etlmaster
-git push -f trader-etl etlmaster:master
-git branch -D etlmaster
--- deploy-ex-stage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git branch -D ahmaster
-git subtree split --prefix ah -b ahmaster
-git push -f ah ahmaster:master
-git branch -D ahmaster
